b6e178192753841bb2e2863e15c22ca57b53385e
Yharim318 <gradylikavec@gmail.com>
2024-04-09 20:20:33 -0400

    Simple Tank

--- Cube-Tank/src/main/java/frc/robot/Constants.java
+++ Cube-Tank/src/main/java/frc/robot/Constants.java
@@ -1,18 +1,19 @@
 package frc.robot;
 
 public class Constants {
+    //TODO: Change everything for each Robot 
     public class Left{
-        public static int Front = 1; //TODO: Change for each Robot 
+        public static int Front = 1; 
         public static Boolean FrontReversed = true;
-        public static int Back = 2; //TODO: Change for each Robot 
+        public static int Back = 2; 
         public static Boolean BackReversed = true;
     }
     public class Right{
-        public static int Front = 3; //TODO: Change for each Robot 
+        public static int Front = 3; 
         public static Boolean FrontReversed = false;
-        public static int Back = 4; //TODO: Change for each Robot 
+        public static int Back = 4; 
         public static Boolean BackReversed = false;
     }
-    public static int DriverPort = 0; //TODO: Change for each Robot
-    public static double StickDeadband = 0.03; //TODO: Change for each Robot
+    public static int DriverPort = 0; 
+    public static double StickDeadband = 0.03; 
 }
