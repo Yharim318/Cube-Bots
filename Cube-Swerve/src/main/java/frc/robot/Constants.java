f3f3224b035da4c58d138082059b6af1dd290e23
Yharim318 <gradylikavec@gmail.com>
2024-04-16 15:40:16 -0400

    ddfdf

--- Cube-Swerve/src/main/java/frc/robot/Constants.java
+++ Cube-Swerve/src/main/java/frc/robot/Constants.java
@@ -4,28 +4,31 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
 /** Add your docs here. */
 public class Constants {
     public class SwerveWheelConstants{
         public class FrontWheels{
-            public int RightDrive = 1;
-            public int LeftDrive = 2;
-            public int RightAngle = 3;
-            public int LeftAngle = 4;
+            public final static int RightDrive = 1;
+            public final static int LeftDrive = 2;
+            public final static int RightAngle = 3;
+            public final static int LeftAngle = 4;
 
-            public int RightCancoder = 5;
-            public int LeftCancoder = 6;
+            public final static int RightCancoder = 5;
+            public final static int LeftCancoder = 6;
 
 
         }
         public class BackWheels{
-            public int RightDrive = 7;
-            public int LeftDrive = 8;
-            public int RightAngle = 9;
-            public int LeftAngle = 10;
+            public final static int RightDrive = 7;
+            public final static int LeftDrive = 8;
+            public final static int RightAngle = 9;
+            public final static int LeftAngle = 10;
 
-            public int RightCancoder = 11;
-            public int LeftCancoder = 12;
+            public final static int RightCancoder = 11;
+            public final static int LeftCancoder = 12;
         }
         public class PIDConstants{
             public final static double kP = 0.085; // Proportion
@@ -33,11 +36,23 @@ public class Constants {
             public final static double kD = 0.001; // Derivative
         }
         public class ChassisConstants{
-            public double length = 1;
-            public double width = 1;
+            public final static double length = 1;
+            public final static double width = 1;
 
-            public double maxDriveSpeed = 1;
-            public double maxRotationSpeed = Math.PI;
+            public final static double maxDriveSpeed = 1;
+            public final static double maxRotationSpeed = Math.PI;
+
+            public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+                new Translation2d(length / 2.0, width / 2.0),
+                new Translation2d(length / 2.0, -width / 2.0),
+                new Translation2d(-length / 2.0, width / 2.0),
+                new Translation2d(-length / 2.0, -width / 2.0));
+        }
+        public enum kMotors{
+            FL,
+            FR,
+            BR,
+            BL
         }
     }
 }
